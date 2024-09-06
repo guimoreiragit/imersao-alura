323469e18c1c169bf19a52037db4955438e57882
Guilherme Moreira <guilhermegustavoc.m@gmail.com>
2024-09-06 14:52:59 -0300

    Talvez um o fim
    projeto imersão alura terminado, a partir  deste é possivel gerar muitos outros ou até mesmo refinar essa base adquirida.

--- Site/app.js
+++ Site/app.js
@@ -1,7 +1,49 @@
-console.log(dados);
+function button() {
+    // Obtém a seção onde os resultados serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
 
-let section = document.getElementById("resultado-pesquisa")
+    let botaoPesquisa = document.getElementById("botao-pesquisa").value;
 
-//console.log(section)
+    // Inicializa uma string vazia para armazenar os resultados
+    let results = "";
+    let titulo = "";
+    let descricao = "";
+    let tag = "";
 
-//section.innerHTML = "resultado-pesquisa"
+
+    // Itera sobre cada dado e adiciona o HTML correspondente aos resultados
+    for (let dado of dados) {
+        titulo = dado.titulo.toLowerCase()
+        descricao = dado.descricao.toLowerCase()
+        tag = dado.tags.toLowerCase()
+
+        if (titulo.includes(botaoPesquisa) || descricao.includes(botaoPesquisa) || tag.includes(botaoPesquisa)) {
+            // Cria um novo elemento div para cada resultado
+            results += `
+        <div class="item-resultado">
+            <h2>${dado.titulo}</h2>
+            <p>${dado.descricao}</p>
+            <a href="${dado.link}" target="_blank">mais informações</a>
+        </div>
+    `;
+
+            if (botaoPesquisa == "") {
+                section.innerHTML = "<p>Nenhum personagem foi encontrado</p>"
+                return false
+            }
+            botaoPesquisa = botaoPesquisa.toLowerCase()
+
+        
+            if (botaoPesquisa == " "){
+                section.innerHTML = "<p>Nenhum personagem foi encontrado</p>"
+                return false
+            }
+        }
+        if (!results){
+            results ="<p>Nenhum personagem foi encontrado</p>"
+        }
+
+        // Atribui o HTML gerado à seção
+        section.innerHTML = results
+    }
+}
--- Site/dados.js
+++ Site/dados.js
@@ -3,21 +3,25 @@ let dados = [
     {
         titulo: "Aerith Gainsborough",
         descricao: "Aerith Gainsborough é uma personagem inesquecível de Final Fantasy VII, conhecida por sua gentileza, esperança e profunda conexão com o planeta. Vendedora de flores em Midgar, Aerith é a última descendente dos Ancients, um povo antigo com poderes místicos. Sua presença traz uma luz de positividade e compaixão ao grupo de Cloud, contrastando com a dureza do mundo em que vivem. Com seus cabelos longos e olhos verdes, Aerith encanta a todos com sua beleza e personalidade única. Apesar de sua natureza doce, ela esconde um destino trágico e um papel crucial na salvação do planeta. Sua relação com Cloud é um dos pontos altos do jogo, marcada por momentos de ternura e profunda conexão emocional. A morte de Aerith é um dos eventos mais marcantes da história dos videogames, deixando uma marca profunda nos jogadores.",
-        link: "https://finalfantasy.fandom.com/wiki/Aerith_Gainsborough"
+        link: "https://finalfantasy.fandom.com/wiki/Aerith_Gainsborough",
+        tags: "airith airi gains florista lirio branco"
     },
     {
         titulo: "Tifa Lockhart",
         descricao: "Tifa Lockhart é uma das personagens mais icônicas de Final Fantasy VII. Amiga de infância de Cloud, ela é uma mulher forte, determinada e habilidosa em artes marciais. Tifa é a alma da AVALANCHE, o grupo de resistência que luta contra a poderosa corporação Shinra, e é uma figura central na jornada de Cloud. Moradora dos subúrbios de Midgar, Tifa possui um forte senso de justiça e compaixão. Sua força interior e sua capacidade de liderar inspiram seus companheiros, e sua relação com Cloud é um dos pilares emocionais da história. Enquanto Cloud luta para descobrir sua verdadeira identidade, Tifa está sempre ao seu lado, oferecendo apoio e encorajamento. Com um passado marcado por tragédia, Tifa carrega consigo as cicatrizes de Nibelheim, sua cidade natal destruída pela Shinra. Apesar das dificuldades, ela se mantém forte e determinada a proteger aqueles que ama. Sua presença é essencial para a jornada do grupo, e sua coragem e determinação a tornam uma personagem inesquecível.",
-        link: "https://finalfantasy.fandom.com/wiki/Tifa_Lockhart"
+        link: "https://finalfantasy.fandom.com/wiki/Tifa_Lockhart",
+        tags: "lutadora punho vermelho atletica soco"
     },
     {
-    titulo: "Cloud Strife",
-    descricao: "Cloud Strife é o protagonista emblemático de Final Fantasy VII. Inicialmente apresentado como um mercenário durão e antissocial, sua jornada o leva por uma profunda exploração de sua identidade e passado. A misteriosa Buster Sword, sua espada icônica, acompanha Cloud em suas aventuras enquanto ele se envolve em uma luta contra a poderosa corporação Shinra e descobre a verdade sobre o projeto SOLDIER, que moldou sua vida de maneiras que ele nunca imaginou. Ao longo da história, Cloud evolui de um personagem solitário para um líder carismático, inspirando seus companheiros e lutando por um futuro melhor para o planeta.",
-    link: "https://finalfantasy.fandom.com/wiki/Cloud_Strife"
+        titulo: "Cloud Strife",
+        descricao: "Cloud Strife é o protagonista emblemático de Final Fantasy VII. Inicialmente apresentado como um mercenário durão e antissocial, sua jornada o leva por uma profunda exploração de sua identidade e passado. A misteriosa Buster Sword, sua espada icônica, acompanha Cloud em suas aventuras enquanto ele se envolve em uma luta contra a poderosa corporação Shinra e descobre a verdade sobre o projeto SOLDIER, que moldou sua vida de maneiras que ele nunca imaginou. Ao longo da história, Cloud evolui de um personagem solitário para um líder carismático, inspirando seus companheiros e lutando por um futuro melhor para o planeta.",
+        link: "https://finalfantasy.fandom.com/wiki/Cloud_Strife",
+        tags: "soldado Soldier SOLDIER edge guerreiro"
     },
     {
-    titulo: "Yuffie Kisaragi",
-    descricao: "Yuffie Kisaragi é uma personagem vibrante e cheia de energia em Final Fantasy VII. Originária da nação de Wutai, ela é uma jovem ninja habilidosa e determinada. Com seu shuriken e seu espírito aventureiro, Yuffie embarca em uma jornada para encontrar um Materia raro para fortalecer sua nação. Sua personalidade descontraída e seu senso de humor contrastam com sua habilidade em combate. Apesar de sua aparência jovem e jovial, Yuffie é uma lutadora formidável, capaz de surpreender seus oponentes com seus rápidos ataques e técnicas de ninjutsu. Sua lealdade a Wutai a impulsiona a superar desafios e a encontrar seu lugar no mundo.",
-    link: "https://finalfantasy.fandom.com/wiki/Yuffie_Kisaragi"
+        titulo: "Yuffie Kisaragi",
+        descricao: "Yuffie Kisaragi é uma personagem vibrante e cheia de energia em Final Fantasy VII. Originária da nação de Wutai, ela é uma jovem ninja habilidosa e determinada. Com seu shuriken e seu espírito aventureiro, Yuffie embarca em uma jornada para encontrar um Materia raro para fortalecer sua nação. Sua personalidade descontraída e seu senso de humor contrastam com sua habilidade em combate. Apesar de sua aparência jovem e jovial, Yuffie é uma lutadora formidável, capaz de surpreender seus oponentes com seus rápidos ataques e técnicas de ninjutsu. Sua lealdade a Wutai a impulsiona a superar desafios e a encontrar seu lugar no mundo.",
+        link: "https://finalfantasy.fandom.com/wiki/Yuffie_Kisaragi",
+        tags: "ninja Ninja kunoichi Yuifie yufi yuf sumidão sumidao"
     }
 ];
